0bd8633f1d12b298b58a19c58a43449018d63685
Diogosr6 <diogosr6@gmail.com>
2018-02-07 15:41:36 +0000

    Added User mongoose model

--- server/server.js
+++ server/server.js
@@ -9,14 +9,46 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 //Modelo de dados para os Todos
 var Todo = mongoose.model('Todo', {
     text: {
-        type: String
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     },
     completedAt: {
-        type: Number
+        type: Number,
+        default: null
     }
+});
+
+//Modelo de dados para os Users
+var User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 1
+    }
+});
+
+var newUser = new User({
+    name: 'Zé Manel',
+    email: 'zemanel@cenas.com'
+});
+
+newUser.save().then( (doc) => {
+    console.log(JSON.stringify(doc, undefined, 2));
+}, (err) => {
+    console.log('Unable to save user', err);
 });
 
 /* var newTodo = new Todo({
@@ -29,14 +61,12 @@ newTodo.save().then((doc) => {
     console.log('Unable to save todo');
 }); */
 
-var otherTodo = new Todo({
-    text: 'Feed the cat',
-    completed: true,
-    completedAt: 123
+/* var otherTodo = new Todo({
+    text: '   Shop dishes    ',
 });
 
 otherTodo.save().then((doc) => {
     console.log(JSON.stringify(doc, undefined, 2));
 }, (err) => {
     console.log('Unable to save other todo', err);
-});
+}); */
